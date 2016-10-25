45d64d47817ddb3941ac702a1cbd3bfe46080dfd
mercury-25 <mercury297@mail.ru>
2016-10-25 14:20:06 +0300

    Add files via upload

---
+++ ProBase.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'Base.php';
+class ProBase extends Base {
+   function show() {
+        echo "ProBase";
+    }
+};
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+ require_once 'ProBase.php';
+   $obj= new Base();
+     echo $obj -> show();
+     echo "</br>";
+      $obj= New ProBase ();
+      echo $obj -> show();
+?>
